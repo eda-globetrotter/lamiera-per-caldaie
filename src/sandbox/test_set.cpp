ca555f4d4cb4b74dfe150a22012e6e7ea6deeb6d
eda-globetrotter <ongz-at-the-domain-of-acm>
2015-09-15 08:55:37 -0500

    Update notes on C++

--- src/sandbox/test_set.cpp
+++ src/sandbox/test_set.cpp
@@ -636,7 +636,7 @@ int main(int argc, char *argv[]) {
 	 * Solution: Add a number field to node_swap and re-test
 	 *	the code calling the find() function.
 	 *
-	 * Fix this!!!
+	 * Fix this!!! Work in progress.
 	 */
 	
 	
