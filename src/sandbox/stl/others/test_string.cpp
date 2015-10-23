7190fb75b192393cfc05ca6f5daf12a4a5783ab1
Zhiyang Ong <ongz@acm.org>
2015-10-22 23:26:43 -0500

    Initial experimentations with C++ strings and string-based nodes/classes

---
+++ src/sandbox/stl/others/test_string.cpp
@@ -0,0 +1,133 @@
+/**
+ * This is written by Zhiyang Ong to test ideas about null pointers.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright	(C)	<2014>	<Zhiyang Ong>
+ *
+ * Permission is hereby granted, free of charge, to any person
+ *	obtaining a copy of this software and associated documentation
+ *	files (the "Software"), to deal in the Software without
+ *	restriction, including without limitation the rights to use, copy,
+ *	modify, merge, publish, distribute, sublicense, and/or sell copies
+ *	of the Software, and to permit persons to whom the Software is
+ *	furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ *	included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *	DEALINGS IN THE SOFTWARE.
+ * 
+ * 
+ *
+ * @author Zhiyang Ong
+ *
+ */
+
+// Import Header files from the C++ STL
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// Import packages from the software
+#include "../../classes/string_node.hpp"
+
+
+using namespace std;
+
+// =======================================================================
+
+// Start of main function...
+int main(int argc, char *argv[]) {
+	// seg fault - accessing something that isn't there
+	// bus error - accessing some thing (data element) that has been freed
+
+	
+	
+	string a_str = "An EECS Ph.D. degree is required for research positions at Intel";
+	string b_str = "Before you become a research fellow, you must be a research scientist at Intel";
+	string duplicate_str = "An EECS Ph.D. degree is required for research positions at Intel";
+	string copy_str = a_str;
+	string *ptr2str = &a_str;
+	
+	/**
+	 * Using "==" to compare two strings checks if the strings
+	 *	point to the same position.
+	 * Using strcmp to compare two strings checks if the strings
+	 *	have the same content.
+	 *
+	 * To use strcmp, convert C++ string to C string via ".c_str()".
+	 *
+	 * Alternatively, use the .compare() function from the C++ STL
+	 *	string class to compare two C++ strings.
+	 */
+
+	if(0 > a_str.compare(b_str)) {
+		cout << "	a_str comes before b_str." << endl;
+	}else{
+		cout << "	a_str DOES NOT come before b_str." << endl;
+	}
+	
+	if(0 < b_str.compare(duplicate_str)) {
+		cout << "	b_str comes after duplicate_str." << endl;
+	}else{
+		cout << "	b_str DOES NOT come after duplicate_str." << endl;
+	}
+	
+	if(0 == a_str.compare(duplicate_str)) {
+		cout << "	a_str is equal to duplicate_str." << endl;
+	}else{
+		cout << "	a_str IS NOT equal to duplicate_str." << endl;
+	}
+	
+	copy_str = "A basketball player is tall.";
+	cout << "	copy_str is:::" << copy_str << "===" << endl;
+	cout << "	a_str is:::" << a_str << "===" << endl;
+	
+	(*ptr2str) = "I love the NBA.";
+	cout << "	*ptr2str is:::" << (*ptr2str) << "===" << endl;
+	cout << "	a_str is:::" << a_str << "===" << endl;
+	
+	// Declaring multiple variables at once. OK.
+	string_node node_a, node_b, node_c;
+	
+	// Does not work.
+	//string_node node_d = string("Ciao", "Mondo");
+	//string_node node_d = string("Ciao".c_str(), "Mondo".c_str());
+	//string_node node_d = string(a_str.c_str(), b_str.c_str());
+	string_node node_d = string_node(a_str, b_str);
+	// The following works.
+	string_node node_e = string_node("Ciao", "Mondo");
+	string_node node_f = string_node(a_str.c_str(), b_str.c_str());
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	// End of main function...
+	return 0;
+}
