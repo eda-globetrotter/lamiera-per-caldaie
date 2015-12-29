a202df67c8935cf2afa404dd335a5c509386b3b7
Zhiyang Ong <ongz@acm.org>
2015-12-29 15:54:43 -0600

    Add code to convert numbers to strings, to the utility package

--- src/utilities/conversion_alphanumeric.hpp
+++ src/utilities/conversion_alphanumeric.hpp
@@ -108,7 +108,7 @@ class conversion_alphanumeric {
 		// Mutator functions.
 		
 		// Function to enter/exit debugging mode
-		static void set_debugging_mode(const bool &debug_mode);
+		static void set_debugging_mode(const bool 
 
 	
 		// -----------------------------------------------------
