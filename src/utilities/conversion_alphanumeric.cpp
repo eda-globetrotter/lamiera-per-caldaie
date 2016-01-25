5e7ebb5e82a17f9643efc25724557329a45044ba
Zhiyang Ong <ongz@acm.org>
2016-01-25 16:48:23 -0600

    Clean up code to convert numbers to strings, in the utility package

--- src/utilities/conversion_alphanumeric.cpp
+++ src/utilities/conversion_alphanumeric.cpp
@@ -97,12 +97,36 @@ conversion_alphanumeric::conversion_alphanumeric() {
 
 // Functions to convert numbers to strings.
 
+
+// For C++11 (and other C++1y) compliant compilers.
+
 /**
- * Function to determine if mode is debugging or otherwise.
- * @param - None
- * @return - boolean TRUE, if in debugging mode; else, return FALSE.
+ * The following functions can be used:
+ *	string to_string (int val);
+ *	string to_string (long val);
+ *	string to_string (long long val);
+ *	string to_string (unsigned val);
+ *	string to_string (unsigned long val);
+ *	string to_string (unsigned long long val);
+ *	string to_string (float val);
+ *	string to_string (double val);
+ *	string to_string (long double val);
+ *
+ * Reference:
+ *
+ *
+ *
+ *
+ *
  */
-bool conversion_alphanumeric::num_to_str(int an_int) {
+
+
+/**
+ * Function to convert an int to a string.
+ * @param a_num:	An int to be converted to a string.
+ * @return string representation of a_num.
+ */
+bool conversion_alphanumeric::num_to_str(int a_num) {
 	if(is_debugging) {
 		return true;
 	}else{
