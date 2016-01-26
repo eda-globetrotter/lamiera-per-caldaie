6896276eb6fe0810b02cc9821d74d6b09e495143
Zhiyang Ong <ongz@acm.org>
2016-01-25 23:12:31 -0600

    Testing code to convert numbers to strings, in the utility package

--- src/test/utilities_t/conversion_alphanumeric_ut.hpp
+++ src/test/utilities_t/conversion_alphanumeric_ut.hpp
@@ -2,28 +2,10 @@
  * Test/Utilities_t package:
  * Submodule of Test package to test classes in the Utilities package.
  *
- * printer_ut class:
- * Class that tests the printer class.
- * The constructors and functions of the printer class shall be tested.
+ * conversion_alphanumeric_ut class:
+ * Class that tests the conversion_alphanumeric class.
+ * The constructors and functions of the conversion_alphanumeric class shall be tested.
  * 
- * Utilities package:
- * Module to carry out miscellaneous tasks to facilitate software
- *	development.
- *
- * conversion_alphanumeric class:
- * Class to convert numbers to strings.
- * The C++ STL provides 2 functions to convert strings to numbers.
- * However, the functions for converting numbers to strings are not
- *	provided by the C++ STL (as part of the C++14 standard).
- * Hence, this class provides some functions for converting numbers
- *	to strings.
- * 
- *
- *
- *
- *
- *
- *
  *
  *
  * @author Zhiyang Ong
@@ -55,31 +37,14 @@
  *
  * Email address: echo "cukj -wb- 23wU4X5M589 TROJANS cqkH wiuz2y 0f Mw Stanford" | awk '{ sub("23wU4X5M589","F.d_c_b. ") sub("Stanford","d0mA1n"); print $5, $2, $8; for (i=1; i<=1; i++) print "6\b"; print $9, $7, $6 }' | sed y/kqcbuHwM62z/gnotrzadqmC/ | tr 'q' ' ' | tr -d [:cntrl:] | tr -d 'ir' | tr y "\n"		Don't compromise my computing accounts. You have been warned.
  *
- *
- *
- *
- *
- *
- *
- *
- * This Class conversion_alphanumeric allows numbers to be converted
- *	to strings.
- *
- * All the functions in this class are static.
- * This is because I do not want to instantiate any instances of this
- *	class.
- * If I need an instance of this class in another class to convert a
- *	number to a string, I would probably leave a lot of objects
- *	lying around that are not being subjected to any garbage
- *	collection.
- * Hence, to minimize dynamic memory usage, I would prevent the
- *	instantiation of objects of this class.
  */
 
 
 // Import packages from the software
-#include "violated_assertion.hpp"
-#include "violated_precondition.hpp"
+// Import Header files from the other modules of lamiera-per-caldaie.
+#include "../../utilities/printer.hpp"
+#include "../../utilities/violated_assertion.hpp"
+#include "../../utilities/violated_precondition.hpp"
 
 
 // Import packages from the C++ STL
@@ -87,14 +52,14 @@
 #include <string>
 #include <cstring>
 
-#ifndef __CONVERSION_ALPHANUMERIC_H
-#define __CONVERSION_ALPHANUMERIC_H
+#ifndef __CONVERSION_ALPHANUMERIC__UT_H
+#define __CONVERSION_ALPHANUMERIC_UT_H
 using namespace std;
 
 // =========================================================
 
-// Class definition of the conversion_alphanumeric class
-class conversion_alphanumeric {
+// Class definition of the conversion_alphanumeric_ut class
+class conversion_alphanumeric_ut {
 	// Publicly accessible data members, constructors, and functions
 	public:
 		// Default constructor
