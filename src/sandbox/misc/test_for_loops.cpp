92cfc652fea9330e0851753fe6a6b913b1b89d6a
Zhiyang Ong <ongz@acm.org>
2016-02-12 15:07:03 -0600

    Test compound assignments

---
+++ src/sandbox/misc/test_for_loops.cpp
@@ -0,0 +1,93 @@
+/**
+ * This is written by Zhiyang Ong to test ideas about "for loops".
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright	(C)	<2014>	<Zhiyang Ong>
+ *
+ * Permission is hereby granted, free of charge, to any person
+ *	obtaining a copy of this software and associated documentation
+ *	files (the "Software"), to deal in the Software without
+ *	restriction, including without limitation the rights to use, copy,
+ *	modify, merge, publish, distribute, sublicense, and/or sell copies
+ *	of the Software, and to permit persons to whom the Software is
+ *	furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ *	included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *	DEALINGS IN THE SOFTWARE.
+ * 
+ * 
+ *
+ * @author Zhiyang Ong
+ *
+ */
+
+// Import Header files from the C++ STL
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <math.h>
+#include <climits>
+#include <cfloat>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+// Testing operations with the C++ STL set container.
+//#include <set>
+
+// Import packages from the software
+//#include "../classes/node.hpp"
+
+
+using namespace std;
+
+// =======================================================================
+
+// Start of main function...
+int main(int argc, char *argv[]) {
+	// seg fault - accessing something that isn't there
+	// bus error - accessing some thing (data element) that has been freed
+
+	/**
+	 * Sequence of "i" values:
+	 *	12.98634
+	 *	16.1099859
+	 *	19.2336318
+	 *	22.3572777
+	 */
+	for (double i=12.98634; i<23.4567; i=i+3.1236459) {
+		cout << "	Value of i:::" << i << endl;
+	}
+	
+	cout << "===========================================" << endl;
+	
+	/**
+	 * Playing with "compound assignments"
+	 *
+	 *
+	 * References:
+	 *	\cite[pp. 22-23]{Soulie2007}.
+	 */
+	int test_int = 23;
+	cout << "	test_int is	:::" << test_int << "===" << endl;
+	test_int+=7;
+	cout << "	test_int+=7 is			:::" << test_int << "===" << endl;
+	test_int =+4;
+	cout << "	test_int =+4 is	:::" << test_int << "===" << endl;
+	cout << "	After test_int=+4:	:::" << test_int << "===" << endl;
+	
+	
+	// End of main function...
+	return 0;
+}
