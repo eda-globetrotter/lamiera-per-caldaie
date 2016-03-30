1834ba1e6956613bec3c8c87a49fd35dd2408414
Zhiyang Ong <ongz@acm.org>
2016-03-30 15:10:32 -0500

    Template files are not linking

--- src/test/temp/old/test_templates.cpp
+++ src/test/temp/old/test_templates.cpp
@@ -49,7 +49,7 @@
 // Import packages from the software
 //#include "../classes/node.hpp"
 //#include "../classes/simple_template.cpp"
-#include "../../elements/trivial_template.hpp"
+#include "../../sandbox/classes/simple_template.hpp"
 
 using namespace std;
 
@@ -64,17 +64,17 @@ int main(int argc, char *argv[]) {
 	/**
 	 * Testing a simple temnplate.
 	 */
-//	cout << "	###" << trivial_template::number_to_string<int>(493) << "===" << endl;
-//	cout << "	###" << trivial_template::is_non_negative<int>(493) << "===" << endl;
-//	cout << "	###" << trivial_template<int>::is_non_negative<int>(493) << "===" << endl;
-//	cout << "	###" << trivial_template<int>::is_non_negative<int>(493) << "===" << endl;
+//	cout << "	###" << simple_template::number_to_string<int>(493) << "===" << endl;
+//	cout << "	###" << simple_template::is_non_negative<int>(493) << "===" << endl;
+//	cout << "	###" << simple_template<int>::is_non_negative<int>(493) << "===" << endl;
+//	cout << "	###" << simple_template<int>::is_non_negative<int>(493) << "===" << endl;
 
 
-	cout << "	###" << trivial_template<int>::is_non_negative(493) << "===" << endl;
-	trivial_template<int> *a_ptr;
+	cout << "	###" << simple_template<int>::is_non_negative(493) << "===" << endl;
+	simple_template<int> *a_ptr;
 	cout << "	###" << a_ptr->square_given_number(5) << "===" << endl;
 	// 0.25^2 = 0.0625
-	trivial_template<long double> *b_ptr;
+	simple_template<long double> *b_ptr;
 	cout << "	###" << b_ptr->square_given_number(-0.25) << "===" << endl;
 	// 16.25^2 = 164.0625
 	cout << "	###" << b_ptr->square_given_number(-16.25) << "===" << endl;
