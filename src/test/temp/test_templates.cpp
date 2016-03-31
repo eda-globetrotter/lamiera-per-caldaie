786ec00e157414893a1d3d6eda24e9ab0056332b
Zhiyang Ong <ongz@acm.org>
2016-03-31 06:53:26 -0500

    Finished fixing linking errors in using C++ templates. See comments of the files for testing templates for more information, including references.

--- src/test/temp/test_templates.cpp
+++ src/test/temp/test_templates.cpp
@@ -9,16 +9,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "../../elements/trivial_template.hpp"
+//#include "../../elements/trivial_template.hpp"
+//#include "../../elements/merged_template.hpp"
+#include "../../elements/double_template.cpp"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-	cout << "	###" << trivial_template<int>::is_non_negative(493) << "===" << endl;
-	trivial_template<int> *a_ptr;
+	cout << "	###" << merged_template<int>::is_non_negative(493) << "===" << endl;
+	merged_template<int> *a_ptr;
 	cout << "	###" << a_ptr->square_given_number(5) << "===" << endl;
 	// 0.25^2 = 0.0625
-	trivial_template<long double> *b_ptr;
+	merged_template<long double> *b_ptr;
 	cout << "	###" << b_ptr->square_given_number(-0.25) << "===" << endl;
 	// 16.25^2 = 164.0625
 	cout << "	###" << b_ptr->square_given_number(-16.25) << "===" << endl;
