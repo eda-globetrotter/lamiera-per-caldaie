46f3d65e85a9ce70b26848273c9fa51d8ebc710a
Zhiyang Ong <ongz@acm.org>
2016-03-31 16:17:12 -0500

    Python script to set up benchmark repository: Version 2.0.

--- scripts/setup_benchmarks.py
+++ scripts/setup_benchmarks.py
@@ -15,16 +15,24 @@
 	That is, check if the benchmarks are valid. 
 
 
-
-
-
+	Notes:
+		The solution from [Damian2010] manually enumerates all files
+			and directories in the specified directory. This is slow
+			and inefficient compared to the use of functions from
+			"The Python Standard Library".
 
 
 	References:
-	???
-
-
-
+	[Damian2010]
+		Kristian Damian, answer to the question ``How to count the number of files in a directory using Python?,'' Stack Exchange Inc., New York, NY, April 13, 2010. Available online from {\it Stack Exchange Inc.: Stack Overflow: Questions} at: \url{http://stackoverflow.com/a/2632298/1531728}; March 31, 2016 was the last accessed date.
+	[Lowis2009]
+		Martin v. L{\"{o}}wis, answer to the question ``Count number of files with certain extension in Python,'' Stack Exchange Inc., New York, NY, August 24, 2009. Available online from {\it Stack Exchange Inc.: Stack Overflow: Questions} at: \url{http://stackoverflow.com/a/1320744/1531728}; March 31, 2016 was the last accessed date.
+	[Luke2011]
+		Luke, answer to the question ``How to count the number of files in a directory using Python?,'' Stack Exchange Inc., New York, NY, November 29, 2011. Available online from {\it Stack Exchange Inc.: Stack Overflow: Questions} at: \url{http://stackoverflow.com/a/8311376/1531728}; March 31, 2016 was the last accessed date.
+	[Miranda2015]
+		Guillermo Pereira Miranda, answer to the question ``How to count the number of files in a directory using Python?,'' Stack Exchange Inc., New York, NY, July 8, 2015. Available online from {\it Stack Exchange Inc.: Stack Overflow: Questions} at: \url{http://stackoverflow.com/a/31297262/1531728}; edited on July 8, 2015; March 31, 2016 was the last accessed date.
+	[okobaka2012]
+		okobaka, answer to the question ``How to count the number of files in a directory using Python?,'' Stack Exchange Inc., New York, NY, May 30, 2012. Available online from {\it Stack Exchange Inc.: Stack Overflow: Questions} at: \url{http://stackoverflow.com/a/10812604/1531728}; March 31, 2016 was the last accessed date.
 
 
 
@@ -51,7 +59,7 @@
 
 #	To access contents of a directory or switch directories.
 #from os import listdir, system, chdir
-from os import listdir, chdir
+from os import listdir, chdir, makedirs, walk
 """
 	To perform operations related to absolute/relative paths,
 		directory names, and file types.
@@ -71,6 +79,20 @@ from sys import argv
 
 #	=============================================================
 
+#	Preamble.
+current_working_directory = "."
+path_to_benchmark_repository = "../benchmarks/"
+path_to_ISPD_2013_contest_benchmark = "../benchmarks/ispd2013"
+
+
+#	=============================================================
+#	Function definitions
+
+#	None at the moment.
+
+
+#	=============================================================
+
 #	Print the header banner for the script.
 
 print "============================================================="
@@ -81,16 +103,20 @@ print "==	Begin setting up the benchmark repository for the boilerplate code bas
 		If not, create a directory for the benchmark repository.
 """
 print "	==>	Checks if the benchmark repository exists."
-if not isdir("../benchmarks/"):
+if not isdir(path_to_benchmark_repository):
 	print "		>>	Create a directory for the benchmark repository."
+	makedirs(path_to_benchmark_repository)
 else:
 	print "		>>	The benchmark repository exists."
+
+
 
 #	--------------------------------------------------------
 
 print "	==>	Checks if the ISPD 2013 contest benchmark directory exists."
-if not isdir("../benchmarks/ispd2013"):
+if not isdir(path_to_ISPD_2013_contest_benchmark):
 	print "		>>	Create a directory for the ISPD 2013 contest benchmarks."
+	makedirs(path_to_ISPD_2013_contest_benchmark)
 else:
 	print "		>>	The ISPD 2013 contest benchmark directory exists."
 
@@ -105,7 +131,27 @@ else:
 		repository.
 """
 print "	==>	Check if ISPD 2013 contest benchmark directory is complete."
-print "		... Checking!"	
+print "		... Checking!"
+#	os.listdir(dir_in)
+#	os.getcwd()
+
+"""
+#	Methods for counting the number of files.
+#	From [Luke2011].
+dirpath, dirnames, filenames = walk(path_to_ISPD_2013_contest_benchmark).next()
+print "Number of regular files in ISPD benchmark subdirectory [Luke2011]:", len(filenames)
+#	From [Miranda2015], which is a modification of [Luke2011].
+filenames = next(walk(path_to_ISPD_2013_contest_benchmark))[2]
+print "Number of regular files in ISPD benchmark subdirectory [Miranda2015]:", len(filenames)
+#	From [okobaka2012], which is a modification of [Luke2011] and [Miranda2015].
+print "Number of regular files in ISPD benchmark subdirectory [okobaka2012]:", len(walk(path_to_ISPD_2013_contest_benchmark).next()[2])
+"""
+#	From 
+#	Is there any regular file in the ISPD 2013 contest benchmark directory?
+if not 0 < len(walk(path_to_ISPD_2013_contest_benchmark).next()[2]):
+	print "		>>	No regular file in the ISPD 2013 contest benchmark directory."
+	print "		>>	Download the ISPD 2013 contest benchmarks."
+
 
 
 
