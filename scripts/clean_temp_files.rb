d6974a718d3c090d04db4942309db86f2413fa94
Zhiyang Ong <ongz@acm.org>
2016-03-31 11:07:45 -0500

    Documented which implementation technique would I use for C++ templates.

--- scripts/clean_temp_files.rb
+++ scripts/clean_temp_files.rb
@@ -1,19 +1,25 @@
 #!/usr/bin/ruby -w
 
-#	This Ruby script is written by Zhiyang Ong to remove temporary files from the repository.
+=begin
+	This Ruby script is written by Zhiyang Ong to remove temporary files from the repository.
 
-#	The MIT License (MIT)
+	The MIT License (MIT)
 
-#	Copyright (c) <2014> <Zhiyang Ong>
+	Copyright (c) <2014> <Zhiyang Ong>
 
-#	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
-#	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-#	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#	Email address: echo "cukj -wb- 23wU4X5M589 TROJANS cqkH wiuz2y 0f Mw Stanford" | awk '{ sub("23wU4X5M589","F.d_c_b. ") sub("Stanford","d0mA1n"); print $5, $2, $8; for (i=1; i<=1; i++) print "6\b"; print $9, $7, $6 }' | sed y/kqcbuHwM62z/gnotrzadqmC/ | tr 'q' ' ' | tr -d [:cntrl:] | tr -d 'ir' | tr y "\n"		Don't compromise my computing accounts. You have been warned.
-
+	Email address: echo "cukj -wb- 23wU4X5M589 TROJANS cqkH wiuz2y 0f Mw Stanford" | awk '{ sub("23wU4X5M589","F.d_c_b. ") sub("Stanford","d0mA1n"); print $5, $2, $8; for (i=1; i<=1; i++) print "6\b"; print $9, $7, $6 }' | sed y/kqcbuHwM62z/gnotrzadqmC/ | tr 'q' ' ' | tr -d [:cntrl:] | tr -d 'ir' | tr y "\n"		Don't compromise my computing accounts. You have been warned.
+	
+	
+	References
+	[Wikipedia contributors, 2016]
+		Wikipedia contributors, ``GCH,'' in {\it Wikipedia, The Free Encyclopedia: Disambiguation pages}, Wikimedia Foundation, San Francisco, CA, January 20, 2016. Available online at: \url{https://en.wikipedia.org/wiki/GCH}; last accessed on March 31, 2016.
+=end
 
 
 
@@ -34,8 +40,13 @@
 # Current search/working directory: "binaries" directory
 search_dir = Dir.pwd + "\/.."
 #search_dir = Dir.pwd
-# Types of source files
-file_extension = [ ".o", ".out", ".exe", ".sizes", ".timing", ".txt" ]
+=begin
+	Types of source files.
+	
+	In [Wikipedia contributors, 2016], the ".gch" "file extension
+		[is] used by GCC for precompiled header files".
+=end
+file_extension = [ ".o", ".out", ".exe", ".sizes", ".timing", ".txt", ".gch" ]
 
 
 
