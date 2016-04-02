c20a6d352072c3303e7694eb2acbbb9a97707133
Zhiyang Ong <ongz@acm.org>
2016-04-02 00:10:17 -0500

    Add benchmarks from ISPD 2013 contest.

---
+++ benchmarks/extra/uncompress_files.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+
+"""
+	This Python script is written by Zhiyang Ong to uncompress tar
+		balls in the Gzip'ed (or gzipped) format.
+	
+	Firstly, it checks if the benchmark repository exists.
+	If not, create a directory for the benchmark repository.
+
+	Secondly, it checks if the set of benchmarks are there.
+	If not, download the set of benchmarks to the benchmark
+		repository.
+
+	Thirdly, parse the benchmarks to validate the benchmarks.
+	That is, check if the benchmarks are valid. 
+
+	Fourthly, check if evaluation scripts are available for each
+		set of benchmarks.
+	If not, download the required evaluation scripts (if any).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	The MIT License (MIT)
+
+	Copyright (c) <2016> <Zhiyang Ong>
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+	Email address: echo "cukj -wb- 23wU4X5M589 TROJANS cqkH wiuz2y 0f Mw Stanford" | awk '{ sub("23wU4X5M589","F.d_c_b. ") sub("Stanford","d0mA1n"); print $5, $2, $8; for (i=1; i<=1; i++) print "6\b"; print $9, $7, $6 }' | sed y/kqcbuHwM62z/gnotrzadqmC/ | tr 'q' ' ' | tr -d [:cntrl:] | tr -d 'ir' | tr y "\n"		Don't compromise my computing accounts. You have been warned.
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
